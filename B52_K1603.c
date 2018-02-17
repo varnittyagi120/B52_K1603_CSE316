792d62f17f58a2daf4604fc7ec30bb345bab269c
varnittyagi120 <varnittyagi1204@gmail.com>
2018-02-17 14:26:42 +0530

    Second commit

--- B52_K1603.c
+++ B52_K1603.c
@@ -6,3 +6,18 @@ int a;
 a=100;  //like a shared variable || data of bank 
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t cond = PTHREAD_COND_INITIALIZER;
+void calculateneed(int P,int R,int need[P][R], int max[P][R],int allocation[P][R])
+{
+    for (int i = 0 ; i < P ; i++)
+        for (int j = 0 ; j < R ; j++)
+            need[i][j] = max[i][j] - allocation[i][j];
+    printf("need of processes\n");
+    for (int i = 0 ; i < P ; i++)
+        {
+          for (int j = 0 ; j < R ; j++)
+              {
+                   printf("%d ",need[i][j]);
+              }
+          printf("\n");
+        }
+}
